c1dfea25750f82e7a21c59268057b88b0ba70701
dinesh999lama <dinesh99981@gmail.com>
2020-07-09 12:45:53 +0545

    first commit in this branch, only basic files no implementation

---
+++ build/CMakeFiles/gr-networking-sink.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gr-networking-sink.dir/gr_networking_source.cpp.o"
+  "gr-networking-sink.pdb"
+  "gr-networking-sink"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gr-networking-sink.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ gr_networking_source.cpp
@@ -0,0 +1,7 @@
+#include <gnuradio/block.h>
+#include <gnuradio/blocks/udp_sink.h>
+
+int main() {
+
+	return 0;
+}
