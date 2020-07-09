1b202d69485fadc14231bf5d6a9014c484ead8db
dinesh999lama <dinesh99981@gmail.com>
2020-07-09 16:59:48 +0545

    new change

--- build/CMakeFiles/gr-networking.dir/DependInfo.cmake
+++ build/CMakeFiles/gr-networking.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/linux/gr-serial/gr_networking_source.cpp" "/home/linux/gr-serial/build/CMakeFiles/gr-networking-sink.dir/gr_networking_source.cpp.o"
+  "/home/linux/gr-serial/gr_networking.cpp" "/home/linux/gr-serial/build/CMakeFiles/gr-networking.dir/gr_networking.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- gr_networking_source.cpp
+++
@@ -1,7 +0,0 @@
-#include <gnuradio/block.h>
-#include <gnuradio/blocks/udp_sink.h>
-
-int main() {
-
-	return 0;
-}
